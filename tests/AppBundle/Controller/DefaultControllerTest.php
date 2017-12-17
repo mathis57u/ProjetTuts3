5c09cbb085eca3fd00500aa0edf6a86bde9c5158
mathis <mathis.aurelien@gmail.com>
2017-12-17 18:40:14 +0100

    test3

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -9,7 +9,6 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-        //kek
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
